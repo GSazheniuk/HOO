2eee957a945a398298f115bd79c5f960571aa856
George Sazheniuk <gsazheniuk@gmail.com>
2014-04-24 22:44:11 +0400

    Added galaxy/stars generation simulation to Admin module.

--- HOO.Admin/Form1.Designer.cs
+++ HOO.Admin/Form1.Designer.cs
@@ -28,12 +28,156 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.labelN = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbN = new System.Windows.Forms.TextBox();
+            this.tbZ = new System.Windows.Forms.TextBox();
+            this.tbY = new System.Windows.Forms.TextBox();
+            this.tbX = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(605, 94);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(243, 434);
+            this.panel3.TabIndex = 13;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(16, 320);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(291, 208);
+            this.panel2.TabIndex = 14;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(16, 94);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(504, 220);
+            this.panel1.TabIndex = 15;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(358, 18);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 12;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // labelN
+            // 
+            this.labelN.AutoSize = true;
+            this.labelN.Location = new System.Drawing.Point(252, 23);
+            this.labelN.Name = "labelN";
+            this.labelN.Size = new System.Drawing.Size(15, 13);
+            this.labelN.TabIndex = 8;
+            this.labelN.Text = "N";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(188, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Z";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(124, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Y";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "X";
+            // 
+            // tbN
+            // 
+            this.tbN.Location = new System.Drawing.Point(208, 20);
+            this.tbN.Name = "tbN";
+            this.tbN.Size = new System.Drawing.Size(38, 20);
+            this.tbN.TabIndex = 4;
+            // 
+            // tbZ
+            // 
+            this.tbZ.Location = new System.Drawing.Point(144, 20);
+            this.tbZ.Name = "tbZ";
+            this.tbZ.Size = new System.Drawing.Size(38, 20);
+            this.tbZ.TabIndex = 5;
+            // 
+            // tbY
+            // 
+            this.tbY.Location = new System.Drawing.Point(80, 20);
+            this.tbY.Name = "tbY";
+            this.tbY.Size = new System.Drawing.Size(38, 20);
+            this.tbY.TabIndex = 6;
+            // 
+            // tbX
+            // 
+            this.tbX.Location = new System.Drawing.Point(16, 20);
+            this.tbX.Name = "tbX";
+            this.tbX.Size = new System.Drawing.Size(38, 20);
+            this.tbX.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(864, 547);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.labelN);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbN);
+            this.Controls.Add(this.tbZ);
+            this.Controls.Add(this.tbY);
+            this.Controls.Add(this.tbX);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Label labelN;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbN;
+        private System.Windows.Forms.TextBox tbZ;
+        private System.Windows.Forms.TextBox tbY;
+        private System.Windows.Forms.TextBox tbX;
     }
 }
 
--- HOO.Admin/Form1.cs
+++ HOO.Admin/Form1.cs
@@ -12,9 +12,74 @@ namespace HOO.Admin
 {
     public partial class Form1 : Form
     {
+        Random rnd;
+        List<Point3D> l = new List<Point3D>();
+        int a = 0;
+        int b = 0;
+        int c = 0;
+
+        public class Point3D
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+            public int Z { get; set; }
+        }
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (Point3D p in l)
+            {
+                e.Graphics.FillRectangle(new SolidBrush(Color.Black), p.X + a, p.Y + b, 1, 1);
+            }
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (Point3D p in l)
+            {
+                e.Graphics.FillRectangle(new SolidBrush(Color.Black), p.Y + b, p.Z + c, 1, 1);
+            }
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (Point3D p in l)
+            {
+                e.Graphics.FillRectangle(new SolidBrush(Color.Black), p.Z + c, p.X + a, 1, 1);
+            }
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            rnd = new Random();
+            int n = int.Parse(tbN.Text);
+            a = int.Parse(tbX.Text);
+            b = int.Parse(tbY.Text);
+            c = int.Parse(tbZ.Text);
+            l = new List<Point3D>();
+
+            while (n > 0)
+            {
+                int x = ((rnd.Next(2) == 0) ? 1 : -1) * rnd.Next(a);
+                int y = ((rnd.Next(2) == 0) ? 1 : -1) * rnd.Next(b);
+                int z = ((rnd.Next(2) == 0) ? 1 : -1) * rnd.Next(c);
+                double d = Math.Pow(x, 2) / Math.Pow(a, 2) + Math.Pow(y, 2) / Math.Pow(b, 2) + Math.Pow(z, 2) / Math.Pow(c, 2);
+
+                if (d <= 1)
+                {
+                    l.Add(new Point3D { X = x, Y = y, Z = z });
+                    n--;
+                }
+            }
+
+            panel1.Invalidate();
+            panel2.Invalidate();
+            panel3.Invalidate();
         }
     }
 }
