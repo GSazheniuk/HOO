1bdd035fca0753f08ae20bca256997496d7e2260
George Sazheniuk <gsazheniuk@gmail.com>
2014-04-29 00:20:04 +0400

    Added dimensions and list of stars to Galaxy class.

--- HOO.Core/Model/Universe/Galaxy.cs
+++ HOO.Core/Model/Universe/Galaxy.cs
@@ -8,6 +8,13 @@ namespace HOO.Core.Model.Universe
 {
     public class Galaxy
     {
+        public string Name { get; set; }
         public BlackHole BlackHole { get; set; }
+
+        public int DimensionX { get; set; }
+        public int DimensionY { get; set; }
+        public int DimensionZ { get; set; }
+
+        public List<Star> Stars { get; set; }
     }
 }
