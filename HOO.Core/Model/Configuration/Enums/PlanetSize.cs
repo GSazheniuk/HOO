d2dbf85580d4ac653845eff0bcbf06ac8a4c3a85
George Sazheniuk <gsazheniuk@gmail.com>
2014-04-28 21:44:15 +0400

    Re-added 2 classes, missing in yesterday commit.

---
+++ HOO.Core/Model/Configuration/Enums/PlanetSize.cs
@@ -0,0 +1,12 @@
+﻿namespace HOO.Core.Model.Configuration.Enums
+{
+    public enum PlanetSize
+    {
+        Tiny = 0,
+        Small = 1,
+        Medium = 2,
+        Large = 3,
+        Huge = 4,
+        MrRandom = 5
+    }
+}
---
+++ HOO.Core/Model/Configuration/Enums/PlanetType.cs
@@ -0,0 +1,15 @@
+﻿namespace HOO.Core.Model.Configuration.Enums
+{
+    public enum PlanetType
+    {
+        Barren = 0,
+        Desert = 1,
+        Tundra = 2,
+        Arid = 3,
+        Swamp = 4,
+        Ocean = 5,
+        Terran = 6,
+        Gaia = 7,
+        MrRandom = 8
+    }
+}
