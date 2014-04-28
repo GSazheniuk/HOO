22eaa37b1317c98959e4dbae30a9f2973984f84d
George Sazheniuk <gsazheniuk@gmail.com>
2014-04-29 00:21:16 +0400

    Added scripts for following tables: 1. Galaxies 2. List of Galaxy names 3. Stars

---
+++ HOO.DB/Scripts/Tables/GalaxyDictionary.sql
@@ -0,0 +1,28 @@
+﻿/* To prevent any potential data loss issues, you should review this script in detail before running it outside the context of the database designer.*/
+BEGIN TRANSACTION
+SET QUOTED_IDENTIFIER ON
+SET ARITHABORT ON
+SET NUMERIC_ROUNDABORT OFF
+SET CONCAT_NULL_YIELDS_NULL ON
+SET ANSI_NULLS ON
+SET ANSI_PADDING ON
+SET ANSI_WARNINGS ON
+COMMIT
+BEGIN TRANSACTION
+GO
+CREATE TABLE dbo.GalaxyDictionary
+	(
+	ID int NOT NULL IDENTITY (1, 1),
+	Name nchar(10) NOT NULL
+	)  ON [PRIMARY]
+GO
+ALTER TABLE dbo.GalaxyDictionary ADD CONSTRAINT
+	PK_GalaxyDictionary PRIMARY KEY CLUSTERED 
+	(
+	ID
+	) WITH( STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+
+GO
+ALTER TABLE dbo.GalaxyDictionary SET (LOCK_ESCALATION = TABLE)
+GO
+COMMIT
