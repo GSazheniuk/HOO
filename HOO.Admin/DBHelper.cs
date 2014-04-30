66cb43edd6b2077e9114902f76bd010bfcdc143c
George <Sazheniuk>
2014-04-30 19:13:58 +0400

    Added prototype for DBHelper class which will be used for communication wih DB from Administrative module.

---
+++ HOO.Admin/DBHelper.cs
@@ -0,0 +1,42 @@
+﻿using HOO.Core.Model.Universe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HOO.Admin
+{
+    public class DBCommandResult
+    {
+        public int ResultCode { get; set; }
+        public string ResultMsg { get; set; }
+    }
+
+    public class DBHelper
+    {
+        #region Constructors
+        public DBHelper()
+        {
+
+        }
+        #endregion
+
+        #region Public Methods
+        public DBCommandResult AddGalaxy(Galaxy gal)
+        {
+            return new DBCommandResult();
+        }
+
+        public DBCommandResult AddStar(Star s)
+        {
+            return new DBCommandResult();
+        }
+
+        public DBCommandResult AddOrbitalBody(StarOrbitalBody sob)
+        {
+            return new DBCommandResult();
+        }
+        #endregion
+    }
+}
