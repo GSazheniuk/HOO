8e49a52a62a22d5b9d4810d3cb45e3db6a7da26c
George <Sazheniuk>
2014-05-01 13:10:59 +0400

    Added master form for Administrative module.

---
+++ HOO.Admin/FormAdminModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace HOO.Admin
+{
+    public partial class FormAdminModule : Form
+    {
+        public FormAdminModule()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitToolsStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
