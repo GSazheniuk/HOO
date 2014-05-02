3976a47ae36a6eacfbb3e68a15af8c9c2464f6b9
George <Sazheniuk>
2014-05-02 19:23:32 +0400

    Added constant to define maximum orbits around the star.

--- HOO.Core/Model/Configuration/ConstantParameters.cs
+++ HOO.Core/Model/Configuration/ConstantParameters.cs
@@ -9,5 +9,6 @@ namespace HOO.Core.Model.Configuration
     public class ConstantParameters
     {
         public const int MaxStarTemperatureLevel = 10;
+        public const int MaxOrbitalBodiesForStar = 10; // value for MrRandom. Actual value = Max -1.
     }
 }
