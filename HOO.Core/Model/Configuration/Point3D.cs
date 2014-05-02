ad07dca902d601ec1a24c58f749d4734884734c6
George <Sazheniuk>
2014-05-02 19:24:08 +0400

    Added Point3D class for 3 dimensional coordinates.

---
+++ HOO.Core/Model/Configuration/Point3D.cs
@@ -0,0 +1,9 @@
+﻿namespace HOO.Core.Model.Configuration
+{
+    public class Point3D
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+    }
+}
