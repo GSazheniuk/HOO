d075c515e335d59d4eb29ba33d4c57ad1d0a47a6
George <Sazheniuk>
2014-06-18 18:34:20 +0400

    Basic classes for Technology Tree

---
+++ HOO.Core/Model/Configuration/Enums/TechnologyGroup.cs
@@ -0,0 +1,14 @@
+﻿namespace HOO.Core.Model.Configuration.Enums
+{
+    public enum TechnologyGroup
+    {
+        Construction = 0,
+        Chemistry = 1,
+        Computers = 2,
+        Physics = 3,
+        Power = 4,
+        Sociology = 5,
+        Biology = 6,
+        ForceFields = 7
+    }
+}
