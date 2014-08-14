4da1523d1fef1e803fe1e6079bbdcc10ced0f390
George <Sazheniuk>
2014-08-14 12:04:33 +0400

    Added class for cryptography services.

---
+++ HOO.ComLib/MrCrypt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HOO.ComLib
+{
+    public class MrCrypt
+    {
+        private System.Security.Cryptography.ECDiffieHellmanCng dhCng = new ECDiffieHellmanCng(256);
+        public MrCrypt()
+        {
+            dhCng.HashAlgorithm = CngAlgorithm.ECDiffieHellmanP256;
+            //dhCng.
+        }
+    }
+}
