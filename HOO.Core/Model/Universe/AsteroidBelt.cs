2baf30537a15ed40696656d85a14d3854cf8ffff
unknown-user <unknown-user@192.168.42.55>
2015-09-29 21:58:16 -0400

    Upgraded all StarOrbitalBody objects to same structure.

--- HOO.Core/Model/Universe/AsteroidBelt.cs
+++ HOO.Core/Model/Universe/AsteroidBelt.cs
@@ -9,7 +9,8 @@ namespace HOO.Core.Model.Universe
 {
     public class AsteroidBelt : StarOrbitalBody
     {
-        public AsteroidDensity Density { get; set; }
+		public AsteroidDensity Density { get; set; }
+		public AsteroidType Type { get; set; }
 
         public AsteroidBelt(Star s)
             : base(s)
--- HOO.Core/Model/Universe/GasGiant.cs
+++ HOO.Core/Model/Universe/GasGiant.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HOO.Core.Model.Configuration.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,10 @@ namespace HOO.Core.Model.Universe
 {
     public class GasGiant : StarOrbitalBody
     {
-        public GasGiant(Star s)
+		public GasGiantSize Size { get; set; }
+		public GasGiantClass Class { get; set; }
+
+		public GasGiant(Star s)
             : base(s)
         {
 
