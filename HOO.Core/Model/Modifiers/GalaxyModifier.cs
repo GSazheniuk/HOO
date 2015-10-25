849960d49c82f102f3af96e216cff43d5b46c080
gsazheniuk <gsazheniuk@gmail.com>
2015-10-25 11:27:00 -0400

    Namespace update

--- HOO.Core/Model/Modifiers/GalaxyModifier.cs
+++ HOO.Core/Model/Modifiers/GalaxyModifier.cs
@@ -5,7 +5,7 @@ namespace HOO.Core.Model.Modifiers
 {
 	public class GalaxyModifier
 	{
-		public Attributes.GalaxyAttribute Attribute{ get; set; }
+		public GalaxyAttribute Attribute{ get; set; }
 		public object ModifierValue { get; set; }
 
 		public GalaxyModifier()
