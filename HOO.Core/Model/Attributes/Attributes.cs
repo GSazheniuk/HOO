80196ce1924d1a77ddd834cd30f3687293c8a49a
gsazheniuk <gsazheniuk@gmail.com>
2015-10-28 22:54:20 -0400

    Added Count property and Keys[] collection.

--- HOO.Core/Model/Attributes/Attributes.cs
+++ HOO.Core/Model/Attributes/Attributes.cs
@@ -12,6 +12,9 @@ namespace HOO.Core.Model
 			this._attributes = new Dictionary<int, object> ();
 		}
 
+		public int Count { get { return this._attributes.Count; } }
+		public int[] Keys { get { return this.Keys; } }
+
 		public object this [int index]
 		{
 			get { return this._attributes [index];}
