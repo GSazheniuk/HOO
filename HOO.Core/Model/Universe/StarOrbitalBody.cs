1304d2d8cb42ffd1e03eb1a714faaf661503254c
gsazheniuk <gsazheniuk@gmail.com>
2015-10-30 23:59:17 -0400

    Added Attributes and Effects

--- HOO.Core/Model/Universe/StarOrbitalBody.cs
+++ HOO.Core/Model/Universe/StarOrbitalBody.cs
@@ -11,13 +11,18 @@ namespace HOO.Core.Model.Universe
         public int Id { get; set; }
         public int OrbitNo { get; set; }
         public Star Star { get; set; }
+		public Attributes Attributes;
+		public Effects Effects;
 		public bool IsLoaded;
 		public bool IsSaved;
+
 
 
         public StarOrbitalBody(Star s)
         {
             this.Star = s;
+			this.Attributes = new Attributes ();
+			this.Effects = new Effects ();
 			this.IsLoaded = this.IsSaved = false;
         }
     }
