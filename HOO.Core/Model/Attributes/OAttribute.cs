cd02addc35005d7fbfe0ab8a2965bf018edbc258
gsazheniuk <gsazheniuk@gmail.com>
2015-11-11 22:33:56 -0500

    Base class for Object Attribute.

---
+++ HOO.Core/Model/Attributes/OAttribute.cs
@@ -0,0 +1,22 @@
+namespace HOO.Core.Model
+{
+	public class OAttribute
+	{
+		public int AID { get; set; }
+		public int AttributeID { get; set; }
+		public int AttributeType { get; set; }
+		public object Value { get; set; }
+		public bool IsSaved;
+		public bool IsLoaded;
+
+		public OAttribute(ObjectAttribute attribute, AttributeTypes attributeType, object value)
+		{
+			this.AID = -1;
+			this.AttributeID = (int)attribute;
+			this.AttributeType = (int)attributeType;
+			this.Value = value;
+			this.IsSaved = this.IsLoaded = false;
+		}
+	}
+}
+
