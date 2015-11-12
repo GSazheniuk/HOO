a766423318da930d92a873f40d62bb717a837383
gsazheniuk <gsazheniuk@gmail.com>
2015-11-11 22:34:25 -0500

    Types of attributes supported.

---
+++ HOO.Core/Model/Attributes/AttributeTypes.cs
@@ -0,0 +1,18 @@
+namespace HOO.Core.Model
+{
+	public enum AttributeTypes
+	{
+		Resource = 0,
+		ResourceCost = 1,
+		ResourceFlatChange = 2,
+		ResourceMultiplierChange = 3,
+		Attribute = 4,
+		AttributeFlatBonus = 5,
+		AtributeMultiplierBonus = 6,
+		EternalRequisite = 7,
+		FiniteRequisite = 8,
+		EternalRequirement = 9,
+		FiniteRequirement = 10
+	}
+}
+
