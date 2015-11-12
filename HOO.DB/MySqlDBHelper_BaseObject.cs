8d98f50b35a7cfbcccb23e2ca146a426480b94ec
gsazheniuk <gsazheniuk@gmail.com>
2015-11-11 22:38:37 -0500

    SaveAttributes instead of SaveBaseProperties now saves all attributes to DB. Added LoadAttributes method.

--- HOO.DB/MySqlDBHelper_BaseObject.cs
+++ HOO.DB/MySqlDBHelper_BaseObject.cs
@@ -13,54 +13,42 @@ namespace HOO.DB
 {
 	public partial class MySqlDBHelper
 	{
-		public DBCommandResult SaveBaseProperties(BaseObject bo)
+		public OAttribute[] LoadAttributes(DataTable table)
+		{
+			if (table != null) {
+				OAttribute[] res = new OAttribute[table.Rows.Count];
+				for (int i=0; i<res.Length; i++) {
+					DataRow dr = table.Rows [i];
+					res [i] = new OAttribute ((ObjectAttribute)dr ["AttributeID"], (AttributeTypes)dr ["AttributeType"], dr ["Value"]);
+				}
+				return res;
+			} else
+				return new OAttribute[0];
+		}
+
+		public DBCommandResult SaveAttributes(BaseObject bo)
 		{
 			DBCommandResult res = new DBCommandResult ();
 
 			try {
 				//Save Attributes
-				for (int i = 0; i<bo.Attributes.Count; i++) {
-					int key = bo.Attributes.Keys [i];
+				OAttribute[] list = bo.Attributes.ChangedAttributes ();
+				for (int i = 0; i<list.Length; i++) {
+					OAttribute key = list [i];
 					//throw new Exception (String.Format("{0}. {1}, {2}, {3}", bo.ObjectType, bo.OBID, key, bo.Attributes [key]));
-					MySqlCommand com = new MySqlCommand ("GM_SaveObjectAttribute", _dg.Connection);
+					MySqlCommand com = new MySqlCommand ("GM_SaveAttribute", _dg.Connection);
 					com.CommandType = CommandType.StoredProcedure;
-					MySqlParameter spAttrId = new MySqlParameter ("pAttrId", key);
-					MySqlParameter spObjectType = new MySqlParameter ("pObjectType", bo.ObjectType);
+					MySqlParameter spAId = new MySqlParameter ("pAID", key.AID);
+					MySqlParameter spAttrId = new MySqlParameter ("pAttrId", key.AttributeID);
+					MySqlParameter spObjectType = new MySqlParameter ("pAttrType", key.AttributeType);
 					MySqlParameter spObjectId = new MySqlParameter ("pObjectId", bo.OBID);
-					MySqlParameter spValue = new MySqlParameter ("pValue", bo.Attributes [key]);
+					MySqlParameter spValue = new MySqlParameter ("pValue", key.Value);
 
-					com.Parameters.AddRange (new MySqlParameter[] { spAttrId, spObjectType, spObjectId, spValue });
+					com.Parameters.AddRange (new MySqlParameter[] { spAId, spAttrId, spObjectType, spObjectId, spValue });
 					_dg.ExecuteCommand (com);
 				}
 
-				//Save Effects
-				for (int i = 0; i<bo.Effects.Count; i++) {
-					int key = bo.Effects.Keys [i];
-					MySqlCommand com = new MySqlCommand ("GM_SaveObjectEffect", _dg.Connection);
-					com.CommandType = CommandType.StoredProcedure;
-					MySqlParameter spAttrId = new MySqlParameter ("pAttrId", key);
-					MySqlParameter spObjectType = new MySqlParameter ("pObjectType", bo.ObjectType);
-					MySqlParameter spObjectId = new MySqlParameter ("pObjectId", bo.OBID);
-					MySqlParameter spValue = new MySqlParameter ("pValue", bo.Effects [key]);
-
-					com.Parameters.AddRange (new MySqlParameter[] { spAttrId, spObjectType, spObjectId, spValue });
-					_dg.ExecuteCommand (com);
-				}
-
-				//Save Requisites
-				for (int i = 0; i<bo.Requisites.Count; i++) {
-					int key = bo.Requisites.Keys [i];
-					MySqlCommand com = new MySqlCommand ("GM_SaveObjectRequisite", _dg.Connection);
-					com.CommandType = CommandType.StoredProcedure;
-					MySqlParameter spReqId = new MySqlParameter ("pReqId", key);
-					MySqlParameter spObjectType = new MySqlParameter ("pObjectType", bo.ObjectType);
-					MySqlParameter spObjectId = new MySqlParameter ("pObjectId", bo.OBID);
-					MySqlParameter spValue = new MySqlParameter ("pValue", bo.Requisites [key]);
-
-					com.Parameters.AddRange (new MySqlParameter[] { spReqId, spObjectType, spObjectId, spValue });
-					_dg.ExecuteCommand (com);
-				}
-
+				bo.Attributes.SaveAll();
 				res.Tag = bo;
 				res.ResultCode = 0;
 				res.ResultMsg = "Ok";
