2a88766c55ce7e2a627b0656e1bbf1111dc31246
gsazheniuk <gsazheniuk@gmail.com>
2015-11-14 00:11:35 -0500

    Added three default attribute to initialization.

--- HOO.SvcLib/Helpers/PlayerHelper.cs
+++ HOO.SvcLib/Helpers/PlayerHelper.cs
@@ -22,6 +22,14 @@ namespace HOO.SvcLib.Helpers
 //			this.Player.Attributes.TotalCredits = 1000;
 //			this.Player.Requisites.Capitol = 1;
 //			this.Player.Effects.Income = 0.01;
+			OAttribute[] attrs = new OAttribute[3];
+			attrs [0] = new OAttribute (ObjectAttribute.NativeCredits, AttributeTypes.Resource, 1000);
+			attrs [1] = new OAttribute (ObjectAttribute.NativeCredits, AttributeTypes.ResourceFlatChange, 1);
+			attrs [2] = new OAttribute (ObjectAttribute.Capitol, AttributeTypes.FiniteRequisite, 1);
+			this.Player.Attributes.Load (attrs);
+			this.Player.Attributes.AddAttribute (attrs [0]);
+			this.Player.Attributes.AddAttribute (attrs [1]);
+			this.Player.Attributes.AddAttribute (attrs [2]);
 		}
 
 		public void Register(string userName, string password, string email)
@@ -44,8 +52,20 @@ namespace HOO.SvcLib.Helpers
 			if (res.ResultCode == 0) {
 				this.Player = (Player)res.Tag;
 				this.Player.IsLoaded = this.Player.IsSaved = true;
+				if (this.Player.Attributes.TotalAttributes == 0)
+					InitDefaultParameters ();
+				//ELSE add missing attributes, if any exists.
 			} else {
 				throw new Exception (res.ResultMsg);
+			}
+		}
+
+		public void Tick()
+		{
+			if (this.Player != null) {
+				foreach (OAttribute oa in this.Player.Attributes.ListByType(AttributeTypes.Resource)) {
+					oa.Value = (double)oa.Value + ((double)this.Player.Attributes.ValueOf (ObjectAttribute.NativeCredits, AttributeTypes.ResourceFlatChange)) / 10;
+				}
 			}
 		}
 	}
